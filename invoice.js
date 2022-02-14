107e827e6cd1bba47d7d481c75980cf42f804e80
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-14 17:49:37 +0600

    first commit

---
+++ invoice.js
@@ -0,0 +1,46 @@
+// for submit button
+const submitButton = document.getElementById('detail-submit-btn');
+ submitButton.addEventListener('click',function () {
+    const buyerDetail = document.getElementById('buyer-details-input');
+    document.getElementById('buyer-info').innerText = buyerDetail.value;
+    buyerDetail.value = "";
+});
+
+// for add button
+const addButton = document.getElementById('add-details-btn');
+addButton.addEventListener('click',function () {
+     const tableInfo = document.getElementById('info-table');
+     const itemName = document.getElementById('item-name-input');
+     const itemPrice = document.getElementById('item-price-input');
+     const itemQuantity = document.getElementById('item-quantity-input');
+     console.log(itemName.value,itemPrice.value,itemQuantity.value);
+     {
+        //     <!-- <tr>
+        //     <th >1</th>
+        //     <td>Mark</td>
+        //     <td>Otto</td>
+        //     <td>@mdo</td>
+        // </tr> -->
+        }
+     const totalPrice =  parseInt(itemPrice.value) * parseInt(itemQuantity.value)  ;
+     const tr = document.createElement('tr') ;
+     const th = document.createElement('th') ;
+     const td1 = document.createElement('td'); 
+     const td2 = document.createElement('td'); 
+     const td3 = document.createElement('td'); 
+
+     th.innerText  = itemName.value;
+     td1.innerText = itemPrice.value;
+     td2.innerText = itemQuantity.value;
+     td3.innerText = totalPrice;
+
+     tr.appendChild(th);
+     tr.appendChild(td1);
+     tr.appendChild(td2);
+     tr.appendChild(td3);
+     tableInfo.appendChild(tr);
+     itemName.value = '';
+     itemPrice.value = '';
+     itemQuantity.value = '';
+});
+
